4d23dbe6673cce827693034ff38c360fcd96cf8d
Winnie Ho <winnie-ho@hotmail.co.uk>
2016-12-17 16:37:00 +0000

    title head made for the main actity

--- RunTrack/app/src/main/java/com/example/user/runtrack/MainActivity.java
+++ RunTrack/app/src/main/java/com/example/user/runtrack/MainActivity.java
@@ -86,16 +86,14 @@ public class MainActivity extends AppCompatActivity {
 
         //Inserting test data
         Log.d("Insert: ", "Inserting..");
-        db.addRun(new Run("run",10,10,5,"Park Run","Long"));
+        db.addRun(new Run("Hunters Bog",10,10,5,"Park Run","Long"));
         db.addRun(new Run("Run Club Intervals",5,10,5,"Johnston Terrace Hills","Session"));
         db.addRun(new Run("After work",7,30,5,"WoL","Long"));
-        db.addRun(new Run("Fox Trail 13k", 13, 65, 5,"Foxlake", "Trail"));
+        db.addRun(new Run("Fox Trail 13k", 13, 65, 5,"Foxlake", "Race"));
 
         //Showing data as a List View through and Array Adapter
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, getAllRuns(db));
         allRunList.setAdapter(adapter);
-
-
     }
 
 
@@ -105,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<Run>runs = db.getAllRuns();
         for (Run run : runs){
-            runLog.add(run.getRunTitle() + "  " + run.getDistance() +"km  " + run.getTime() + "mins  ");
+            runLog.add("- " + run.getRunTitle() + "  |  " + run.getDistance() +"km  |  " + run.getTime() + "mins  |  " + run.getType() );
             Log.d("Showing:", "Showing" + run.getRunTitle());
         }
         return runLog;
