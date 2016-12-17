7384a685a67c981aa47012ac59fef80044eaef82
Winnie Ho <winnie-ho@hotmail.co.uk>
2016-12-17 15:43:28 +0000

    some android experiments with styling

--- RunTrack/app/src/main/java/com/example/user/runtrack/MainActivity.java
+++ RunTrack/app/src/main/java/com/example/user/runtrack/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.user.runtrack;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -11,6 +12,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -26,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     EditText titleEditText;
     EditText distanceEditText;
     Button addRunButton;
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
