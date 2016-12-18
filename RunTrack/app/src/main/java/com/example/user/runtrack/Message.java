e29df11855aaf5442ecfa77b02bf2f9ce34b032a
Winnie Ho <winnie-ho@hotmail.co.uk>
2016-12-18 13:17:53 +0000

    messages formatted and more added

--- RunTrack/app/src/main/java/com/example/user/runtrack/Message.java
+++ RunTrack/app/src/main/java/com/example/user/runtrack/Message.java
@@ -16,10 +16,15 @@ public class Message {
     }
 
     public void populateMessages(){
-        messages.add("You're doing great!");
-        messages.add("Keep it going!");
-        messages.add("Power on!");
-        messages.add("Get off your butt!");
+        messages.add("YOU'RE DOING GREAT!");
+        messages.add("KEEP IT GOING!");
+        messages.add("POWER ON!");
+        messages.add("GET OFF YOUR BUTT!");
+        messages.add("SO MANY MILES TO RUN SO LITTLE TIME!");
+        messages.add("THOSE TRAINERS AIN'T GONNA RUN THEMSELVES!");
+        messages.add("IT'S MEANT TO HURT!");
+        messages.add("YOUR COMPETITORS AIN'T RESTING!");
+        messages.add("THAT'S NOT AN EXCUSE!");
     }
 
     public int messagesSize(){
