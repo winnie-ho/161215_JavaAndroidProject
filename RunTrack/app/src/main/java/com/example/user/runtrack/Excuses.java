f459a95cc41992103b2677b42a26e0da8275d9d4
Winnie Ho <winnie-ho@hotmail.co.uk>
2016-12-18 14:22:57 +0000

    excuses page started

--- RunTrack/app/src/main/java/com/example/user/runtrack/Excuses.java
+++ RunTrack/app/src/main/java/com/example/user/runtrack/Excuses.java
@@ -1,17 +1,20 @@
 package com.example.user.runtrack;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by user on 18/12/2016.
  */
 public class Excuses extends AppCompatActivity {
-    Button upForItButton;
-    Button excusesButton;
+    TextView getExcuseTextView;
+    Button giveExcuseButton;
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -37,5 +40,24 @@ public class Excuses extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.excuses);
+
+        getExcuseTextView = (TextView)findViewById(R.id.excuse_text);
+        giveExcuseButton = (Button)findViewById(R.id.give_excuse);
+
+//        giveExcuseButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(Excuses.this, RandomRun.class);
+//                startActivity(intent);
+//            }
+//        });
+
+
     }
 }
