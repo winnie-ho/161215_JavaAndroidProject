ba6f78b9772951106287373486e44870b6f2c6c4
Winnie Ho <winnie-ho@hotmail.co.uk>
2016-12-24 13:57:19 +0000

    some monthSpinner work on the Run Log activity view

--- RunTrack/app/src/main/java/com/example/user/runtrack/AllRuns.java
+++ RunTrack/app/src/main/java/com/example/user/runtrack/AllRuns.java
@@ -1,5 +1,6 @@
 package com.example.user.runtrack;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -32,30 +33,27 @@ public class AllRuns extends AppCompatActivity {
     TextView totalTimeTextView;
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.activity_main,menu);
-        return true ;
+        menuInflater.inflate(R.menu.activity_main, menu);
+        return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(android.view.MenuItem item){
-        if (item.getItemId() == R.id.add_run){
+    public boolean onOptionsItemSelected(android.view.MenuItem item) {
+        if (item.getItemId() == R.id.add_run) {
             Intent intent = new Intent(AllRuns.this, NewRun.class);
             this.startActivity(intent);
             return true;
-        }
-        else if (item.getItemId() == R.id.all_runs){
+        } else if (item.getItemId() == R.id.all_runs) {
             Intent intent = new Intent(AllRuns.this, AllRuns.class);
             this.startActivity(intent);
             return true;
-        }
-        else if (item.getItemId() == R.id.challenge_select){
+        } else if (item.getItemId() == R.id.challenge_select) {
             Intent intent = new Intent(AllRuns.this, ChallengeSelect.class);
             this.startActivity(intent);
             return true;
-        }
-        else if (item.getItemId() == R.id.home){
+        } else if (item.getItemId() == R.id.home) {
             Intent intent = new Intent(AllRuns.this, MainActivity.class);
             this.startActivity(intent);
             return true;
@@ -74,24 +72,27 @@ public class AllRuns extends AppCompatActivity {
         final DBHandler db = ((MainApplication) getApplication()).db;
 
         //Allocating Activity Items an ID from all_runs
-        monthSpinner = (Spinner)findViewById(R.id.month_spinner);
+        monthSpinner = (Spinner) findViewById(R.id.month_spinner);
         allRunList = (ListView) findViewById(R.id.run_list);
         addRun = (Button) findViewById(R.id.button_newRun);
         titleEditText = (EditText) findViewById(R.id.run_title);
         distanceEditText = (EditText) findViewById(R.id.distance);
-        totalRunTextView = (TextView)findViewById(R.id.total_runs);
-        totalDistanceTextView = (TextView)findViewById(R.id.total_distance);
-        totalTimeTextView = (TextView)findViewById(R.id.total_time);
+        totalRunTextView = (TextView) findViewById(R.id.total_runs);
+        totalDistanceTextView = (TextView) findViewById(R.id.total_distance);
+        totalTimeTextView = (TextView) findViewById(R.id.total_time);
 
         totalRunTextView.setText("RUNS \n" + db.getTotalRun());
-        totalDistanceTextView.setText("DISTANCE \n" + db.getTotalDistance()+ " km");
-        totalTimeTextView.setText("TIME \n" + db.getTotalTime()+ " mins");
+        totalDistanceTextView.setText("DISTANCE \n" + db.getTotalDistance() + " km");
+        totalTimeTextView.setText("TIME \n" + db.getTotalTime() + " mins");
+
+        monthSpinner.setOnItemSelectedListener(new SpinnerActivity());
+
 
         //Add New Run Button
         addRun.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d ("Runs: ", "new run button clicked");
+                Log.d("Runs: ", "new run button clicked");
                 Intent intent = new Intent(AllRuns.this, NewRun.class);
                 startActivity(intent);
             }
@@ -108,48 +109,51 @@ public class AllRuns extends AppCompatActivity {
                 String selected = (String) allRunList.getItemAtPosition(position);
                 Log.d("ListView:", selected + " selected, position: " + position + ", id: " + id);
 
-                Run selectedRun = db.getRun(position+1);
+                Run selectedRun = db.getRun(position + 1);
                 Log.d("Selected Run: ", "Selected run: " + selectedRun);
 
                 Intent intent = new Intent(AllRuns.this, ShowRun.class);
 
                 Log.d("SelectedRunInfo", "Selected run: " + selectedRun.getId() + ", " + selectedRun.getRunTitle() +
-                        ", "  + selectedRun.getDistance() + ", " + selectedRun.getTime() + ", " +
+                        ", " + selectedRun.getDistance() + ", " + selectedRun.getTime() + ", " +
                         selectedRun.getPace() + ", " + selectedRun.getRoute() + ", " + selectedRun.getType());
 
                 intent.putExtra("ID", selectedRun.getId());
-                intent.putExtra("Title",selectedRun.getRunTitle());
-                intent.putExtra("Day",selectedRun.getDay());
-                intent.putExtra("Month",selectedRun.getMonth());
-                intent.putExtra("Year",selectedRun.getYear());
-                intent.putExtra("Distance",selectedRun.getDistance());
-                intent.putExtra("Time",selectedRun.getTime());
-                intent.putExtra("Pace",selectedRun.getPace());
-                intent.putExtra("Route",selectedRun.getRoute());
-                intent.putExtra("Type",selectedRun.getType());
+                intent.putExtra("Title", selectedRun.getRunTitle());
+                intent.putExtra("Day", selectedRun.getDay());
+                intent.putExtra("Month", selectedRun.getMonth());
+                intent.putExtra("Year", selectedRun.getYear());
+                intent.putExtra("Distance", selectedRun.getDistance());
+                intent.putExtra("Time", selectedRun.getTime());
+                intent.putExtra("Pace", selectedRun.getPace());
+                intent.putExtra("Route", selectedRun.getRoute());
+                intent.putExtra("Type", selectedRun.getType());
                 startActivity(intent);
             }
         });
 
+
         ArrayAdapter<CharSequence> adapterMonth = ArrayAdapter.createFromResource(this,
                 R.array.month_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+
+        // Specify the layout to use when the list of choices appears
         adapterMonth.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+
+        // Apply the adapter to the spinner
         monthSpinner.setAdapter(adapterMonth);
 
     }
 
     //Accessing all Runs in DB and returning all runs in runLog to make available for Array List
-    private ArrayList<String>getAllRuns(DBHandler db){
+    private ArrayList<String> getAllRuns(DBHandler db) {
         ArrayList<String> runLog = new ArrayList<String>();
 
-        ArrayList<Run>runs = db.getAllRuns();
-        for (Run run : runs){
-            runLog.add(run.getRunTitle() + "  |  " + run.getDistance() +"km  |  " +
-                    run.getTime() + "mins");
-            Log.d("Showing:", "Showing" + run.getRunTitle() );
+        ArrayList<Run> runs = db.getAllRuns();
+        for (Run run : runs) {
+            runLog.add(run.getDay() + "/" + run.getMonth() + "/" + run.getYear() + "         " + run.getRunTitle() + "        " + run.getDistance() + "km");
+            Log.d("Showing:", "Showing" + run.getRunTitle());
         }
         return runLog;
     }
+
 }
